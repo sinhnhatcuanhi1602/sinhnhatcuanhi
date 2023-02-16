d99b90770556a8e454701c6e753e49ca6da95015
sinhnhatcuanhi1602 <125565123+sinhnhatcuanhi1602@users.noreply.github.com>
2023-02-16 21:58:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
     introTitle: ' Chào Nhi!! ',
-    introDesc: `Quỳnh đã đứng đợi Nhi từ sáng tới chiều và có chuyện muốn nói🤭`,
+    introDesc: `Quỳnh đã đứng đợi Nhi từ sáng tới chiều và có chuyện muốn nói🤭, sẵn sàng nghe không???`,
     btnIntro: 'Ok',
     title: 'Hôm nay là ngày thật đặt biệt, của một cô gái tròn 23 tuổi😎',
     desc: 'Quỳnh chỉ muốn nói là... Mà đố Nhi, Quỳnh nói gì đó!...',
@@ -11,7 +11,7 @@ const CONFIG = {
     reasonPlaceholder: '23 tuổi rồi đó, biết là sắp tới có nhiều khó khăn và thử thách, Quỳnh chỉ biết chúc Nhi thật nhiều năng lượng, thật nhiều tiếng cười vui vẻ trong cuộc sống',
     btnReply: 'Send',
     reply: '',
-    mess: 'Đó xong rồi đó！ Gửi những lời tốt đẹp tới tuổi 23!',
-    messDesc: 'Sinh nhật zui zẻ nhaaa. Và',
+    mess: 'Đó xong rồi đó！Gửi những lời tốt đẹp tới tuổi 23!',
+    messDesc: 'Sinh nhật thật hạnh phúc và zui zẻ nhaaa,',
     btnAccept: 'Ok',
 }
