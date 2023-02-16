fc3a35c4e94e7cd366d61eb8049f021480b0c569
Minh Trọng Nguyễn <minhnguyen@adnovum.vn>
2023-02-16 19:16:14 +0700

    init

---
+++ config.js
@@ -0,0 +1,17 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '哈囉 ',
+    introDesc: `今天是個蠻特別的日子。。。我有事想跟你講的，你想不想聽🤭`,
+    btnIntro: 'OK',
+    title: '今天是你的生日，你猜猜我在想什麼的啊😎',
+    desc: 'Uhm，讓我知道你的答案。^_^> ',
+    btnYes: 'uhm, 我猜猜 ',
+    btnNo: '不知道啦，猜不出來',
+    question:'也許是',
+    reasonPlaceholder: '在這兒輸入你的答案...',
+    btnReply: 'Send',
+    reply: 'Murvin, 祝你生日快樂，趕早達到自己的目標，每天都愉快、笑著.27歲是個不小也不大的年齡，加油',
+    mess: 'Wow，猜對了。你看到了沒，我們很投契！',
+    messDesc: 'Đó, xong rồi đó.... Sinh nhật zui zẻ nhaaa.',
+    btnAccept: 'Ok',
+}
